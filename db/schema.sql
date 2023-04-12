4aecdf655bb10e21d8628b2ecf452567529566ef
James A Cone <jacone626@gmail.com>
2023-04-12 16:18:42 -0400

    setup most of schema table and added packages

--- db/schema.sql
+++ db/schema.sql
@@ -3,30 +3,29 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- department
 
--- id: INT PRIMARY KEY
+CREATE TABLE department (
+id INT NOT NULL PRIMARY KEY,
+name VARCHAR(30) to hold department name
+);
 
--- name: VARCHAR(30) to hold department name
+CREATE TABLE role (
+id INT NOT NULL PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
+);
 
--- role
-
--- id: INT PRIMARY KEY
-
--- title: VARCHAR(30) to hold role title
-
--- salary: DECIMAL to hold role salary
-
--- department_id: INT to hold reference to department role belongs to
-
--- employee
-
--- id: INT PRIMARY KEY
-
--- first_name: VARCHAR(30) to hold employee first name
-
--- last_name: VARCHAR(30) to hold employee last name
-
--- role_id: INT to hold reference to employee role
-
--- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE employee (
+id INT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+FOREIGN KEY (role_id)
+REFERENCES role(id)
+ON DELETE SET NULL,
+manager_id INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+);
