d783e33465534bd4e685e90be3bacd5618b7b5a7
James A Cone <jacone626@gmail.com>
2023-04-12 16:33:35 -0400

    added basic info to the server.js file based on previous lessons

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'courses_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+
+
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
